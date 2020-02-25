d66ff9f4c390bcc891b601568710e03f6dd1c24f
Venu Gopal Boddu <vb19419@corp.ojas-it.com>
2020-02-25 19:08:41 +0530

    yes

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { OjasService } from '../ojas.service';
 
 @Component({
   selector: 'app-team',
@@ -12,7 +13,9 @@ export class TeamComponent implements OnInit {
   val: boolean;
   name: any;
   pass: any;
-  constructor(private fb: FormBuilder, private router: Router) { }
+  data: any;
+  status: boolean = false;
+  constructor(private fb: FormBuilder, private router: Router, private s: OjasService) { }
   form = this.fb.group({
     uname: ['', Validators.required],
     password: ['', Validators.required]
@@ -20,31 +23,21 @@ export class TeamComponent implements OnInit {
   ngOnInit() {
   }
   get f() { return this.form.controls; }
-
   onSubmit() {
     this.submitted = true;
     this.name = this.form.controls['uname'].value,
       this.pass = this.form.controls['password'].value
-    if (this.form.invalid) {
-      return;
+    this.s.getDetails().subscribe((res) => {
+      console.log("reg", res[0]);
+      this.data = res;
+    });
+    for(let i = 0; i < this.data.length; i++) {
+      if (this.data[i].uname == this.name && this.data[i].password == this.pass) {
+        alert("login successfull");
+        this.status = true;
+      }
     }
-    else if (this.name == "admin" && this.pass == "admin") {
-      this.router.navigate(['/admin']);
-      this.val = false;
-    } 
-    else if (this.name == "manager" && this.pass == "manager") {
-      this.router.navigate(['/manager']);
-      this.val = false;
-    }
-    else if (this.name == "teamlead" && this.pass == "teamlead") {
-      this.router.navigate(['/teamlead']);
-      this.val = false;
-    }
-    else if (this.name == "employee" && this.pass == "employee") {
-      this.router.navigate(['/employee']);
-      this.val = false;
-    }
-   else {
+    if (this.status == false) {
       this.val = true;
       this.form.reset();
       this.submitted = false;
